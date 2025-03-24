400133a0ef4ec3a765aed59d34216bb09a9134fa
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-24 11:28:11 +0200

    first commit and add new component name equation

---
+++ src/app/equation/equation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equation',
+  standalone: false,
+  templateUrl: './equation.component.html',
+  styleUrl: './equation.component.css'
+})
+export class EquationComponent {
+
+}
