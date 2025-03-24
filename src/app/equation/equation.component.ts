1a6cb38e42d9105e1371ca31306f7d24bce226b7
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-24 11:56:55 +0200

    implement function to display form value

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-equation',
   standalone: false,
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './equation.component.css'
 })
 export class EquationComponent {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl('')
+  })
+
+  get a(){
+    return this.mathForm.value.a;
+
+  }get b(){
+    return this.mathForm.value.b;
+  }
+
+
+
+  randomNumber(){
+    return Math.floor(Math.random()*10);
+  }
+
 
 }
